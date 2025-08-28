5bb91cc45f53a786dcd769560a2cfe0b3f443972
williamthome <williamthome@hotmail.com>
2025-08-28 14:57:53 -0300

    Fix HTML injection with combined setting
    Add injection.combined to HTML injection query to resolve parsing issues with
    HTML content in Arizona templates. This ensures proper HTML syntax highlighting
    for static content while maintaining Erlang injection for dynamic blocks.

--- queries/injections.scm
+++ queries/injections.scm
@@ -2,7 +2,8 @@
 
 ; HTML injection for static content
 ((static) @injection.content
- (#set! injection.language "html"))
+ (#set! injection.language "html")
+ (#set! injection.combined))
 
 ; Erlang injection for dynamic content
 ((dynamic) @injection.content
